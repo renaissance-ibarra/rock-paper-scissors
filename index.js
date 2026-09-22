88b393713b0d89debd0e28ad76890325369876aa
Renaissance Ibarra <renaissanceibarra.worx@gmail.com>
2026-09-22 10:07:05 +0800

    modified:   index.js
    - Created a function that generates computer choice.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+	let randomNum = Math.floor(Math.random() * 3) + 1;
+
+	if (randomNum === 1) {
+		console.log("com chooses rock");
+		return "rock";
+	} else if (randomNum === 2) {
+		console.log("com chooses paper");
+		return "paper";
+	} else if (randomNum === 3) {
+		console.log("com chooses scissors");
+		return "scissors";
+	} else {
+		console.log("invalid number generated");
+		return;
+	}
+}
